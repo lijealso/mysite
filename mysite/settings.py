eedd88ac21c1c906969df287aeb3ddfd91ff9131
lijealso <lijealso@gmail.com>
2024-05-26 23:04:50 +0000

    Database setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        "USER": "django_user",
+        "PASSWORD": "uLDG4J^gtlaVmwnF",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
